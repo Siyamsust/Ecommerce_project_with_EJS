3dde731a7cd67c54cbecf2cae2eed0acbc2c6c5c
Siyamsust <ahamedsiyam43@gmail.com>
2024-10-13 21:13:00 +0600

    change the way of rendering by adding necessary  folder

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,6 @@
   const Product=require('../models/product')
   exports.getAddproduct=(req, res, next) => {
-    res.render('add-product', {
+    res.render('admin/add-product', {
       pageTitle: 'Add Product',
       path: '/admin/add-product',
       formsCSS: true,
@@ -16,7 +16,7 @@
   }
   exports.getProducts=(req, res, next) => {
    Product.fetchAll((products)=>{  //this will now fetch the products from the data folder
-        res.render('shop', {
+        res.render('shop/product-list', {
             prods: products,
             pageTitle: 'Shop',
             path: '/',
--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const productsController = require('../controllers/products');
-const adminData = require('./admin');
+//const adminData = require('./admin');
 
 const router = express.Router();
 
