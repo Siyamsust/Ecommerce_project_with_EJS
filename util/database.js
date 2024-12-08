cd051d213057f38edb91a9c32897880821f9123c
Siyamsust <ahamedsiyam43@gmail.com>
2024-12-08 16:30:29 +0600

    some changes

--- util/database.js
+++ util/database.js
@@ -1,7 +1,26 @@
-const mysql=require('mysql2');
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node',
-})
-module.exports=pool.promise();
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+let _db;
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://siyam_83:LOCket43@cluster0.8nzfq.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db=client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+const getDb=()=>{
+  if(_db){
+    return _db;
+  }
+  throw 'No  database found'
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb=getDb
