1c1cfaa156ffae4cecbe4889ce3f8ade327f1443
Siyamsust <ahamedsiyam43@gmail.com>
2024-12-29 21:56:21 +0600

    done with authentication and password reset

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const nodeMailer = require('nodemailer');
 const sibApiV3Sdk = require('sib-api-v3-sdk');
 const crypto = require('crypto');
+const { reset } = require('mongodb/lib/core/connection/logger');
 require('dotenv').config();
 const client = sibApiV3Sdk.ApiClient.instance;
 const apiKey = client.authentications['api-key'];
@@ -198,7 +199,34 @@ exports.getNewPassword =(req,res,next)=>{
     //isAuthenticated: false,
     errorMessage: message,
     userId:user._id.toString(),
+    passwordToken:token
   });
   
 }).catch(err=>{console.log(err);});
+};
+exports.postNewPassword =(req,res,next)=>{
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+  User.findOne({
+    resetToken:passwordToken,
+    resetTokenExpiration:{$gt:Date.now()},
+    _id:userId
+  })
+  .then(user=>{
+    resetUser = user;
+    return bcrypt.hash(newPassword,12);
+
+  }).then(hashedPassword=>{
+   resetUser.password = hashedPassword;
+   resetUser.resetToken = undefined;
+    resetUser.resetTokenExpiration = undefined;
+    return resetUser.save();
+
+  }).then(result=>{
+    res.redirect('/login');
+  }).catch(err=>{
+    console.log(err);
+  })
 }
