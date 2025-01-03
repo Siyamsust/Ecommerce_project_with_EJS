2fe9178f02f243945b2c63cb4635ea064c7aaf41
Siyamsust <ahamedsiyam43@gmail.com>
2025-01-03 19:59:30 +0600

    file system showing some invalid message

--- routes/admin.js
+++ routes/admin.js
@@ -1,24 +1,54 @@
 const path = require('path');
 
 const express = require('express');
+const { body } = require('express-validator');
 
 const adminController = require('../controllers/admin');
-const is_auth=require('../middleware/isauth');
+const isAuth = require('../middleware/is-auth');
+
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product',is_auth, adminController.getAddProduct);
+router.get('/add-product', isAuth, adminController.getAddProduct);
 
 // /admin/products => GET
-router.get('/products',is_auth, adminController.getProducts);
+router.get('/products', isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product',is_auth, adminController.postAddProduct);
+router.post(
+  '/add-product',
+  [
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postAddProduct
+);
 
-router.get('/edit-product/:productId',is_auth, adminController.getEditProduct);
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product',is_auth, adminController.postEditProduct);
+router.post(
+  '/edit-product',
+  [
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postEditProduct
+);
 
-router.post('/delete-product',is_auth, adminController.postDeleteProduct);
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
--- routes/auth.js
+++ routes/auth.js
@@ -1,33 +1,77 @@
 const express = require('express');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
-//const isauth = require('../middleware/isauth');
-const {check,body}=require('express-validator');
+const User = require('../models/user');
+
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
 
-router.post('/login', authController.postLogin);
 router.get('/signup', authController.getSignup);
-router.post('/signup',[
-    check('email')
-    .isEmail()
-    .withMessage('Enter a valid email'),
-    body('phone','phone number must be 11 characters')
-    .matches(/^[0-9]{11}$/),
-    body('password','please enter a password with 5 characters')
-    .isLength({min: 5}),
-body('confirmPassword').custom((value,{req})=>{
- if(value!==req.body.password){
-     throw new Error('passwords have to match');
- }
-return true;
 
-}) ],authController.postSignup);  
+router.post(
+  '/login',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email address.')
+      .normalizeEmail(),
+    body('password', 'Password has to be valid.')
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim()
+  ],
+  authController.postLogin
+);
+
+router.post(
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        // if (value === 'test@test.com') {
+        //   throw new Error('This email address if forbidden.');
+        // }
+        // return true;
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject(
+              'E-Mail exists already, please pick a different one.'
+            );
+          }
+        });
+      })
+      .normalizeEmail(),
+    body(
+      'password',
+      'Please enter a password with only numbers and text and at least 5 characters.'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+    body('confirmPassword')
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!');
+        }
+        return true;
+      })
+  ],
+  authController.postSignup
+);
+
 router.post('/logout', authController.postLogout);
+
 router.get('/reset', authController.getReset);
+
 router.post('/reset', authController.postReset);
-router.get('/reset/:token',authController.getNewPassword);
-router.post('/new-password', body('password','please enter a password with 5 characters')
-.isLength({min: 5}),authController.postNewPassword);
+
+router.get('/reset/:token', authController.getNewPassword);
+
+router.post('/new-password', authController.postNewPassword);
+
 module.exports = router;
